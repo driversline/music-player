f0796ff6684f169251a6c8119f099c1695632aca
driversline <n526258360@gmail.com>
2025-02-13 02:00:59 +0300

    install.zsh

---
+++ Install/install.zsh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+
+set -e
+
+declare -A files_to_move=(
+    ["$HOME/player/mpd.conf"]="$HOME/.config/mpd"
+    ["$HOME/player/database"]="$HOME/.config/mpd"
+    ["$HOME/player/config"]="$HOME/.config/ncmpcpp"
+    ["$HOME/player/*.mp3"]="$HOME/Music"
+)
+
+directories=(
+    "$HOME/.config/mpd"
+    "$HOME/.config/ncmpcpp"
+    "$HOME/Music"
+)
+
+create_directories() {
+    for dir in "${directories[@]}"; do
+        mkdir -p "$dir"
+    done
+}
+
+move_files() {
+    for src in "${!files_to_move[@]}"; do
+        dest="${files_to_move[$src]}"
+        mv "$src" "$dest"
+    done
+}
+
+main() {
+    create_directories
+    move_files
+}
+
+main
+
+exit 0
